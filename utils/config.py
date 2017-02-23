4451b639c1876b07e0d84b66be274b39f22b4579
Scott Idler <scott.a.idler@gmail.com>
2017-02-22 16:50:31 -0800

    initial add of thin wrapper around search tools like grep and ag for irccloud logs

---
+++ utils/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+loads config.yml files
+'''
+
+import os
+from ruamel import yaml
+from attrdict import AttrDict
+
+CFG_FILES = [
+    '{0}/../config.yml'.format(os.path.dirname(__file__)),
+    '~/.config/irccloud-search/config.yml',
+]
+
+def _load_config(cfgs):
+    config = {}
+    for cfg in cfgs:
+        cfg = os.path.expanduser(cfg)
+        if os.path.isfile(cfg):
+            with open(cfg, 'r') as f:
+                yml = yaml.safe_load(f)
+                if yml:
+                    config.update(yml)
+    return AttrDict(config)
+
+CFG = _load_config(CFG_FILES)
